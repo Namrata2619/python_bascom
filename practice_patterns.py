ab101e70aab7f9d58fe3be08038cf4cbee28550b
Namrata2619 <drnamratapandya@gmail.com>
2024-07-09 07:53:24 +0530

    for input

--- practice_patterns.py
+++ practice_patterns.py
@@ -72,7 +72,7 @@ y = 0
 for x in range(1,6):
     
     for y in range(1,6):
-        print("*",end="") # 
+        print("*",end=" ") # 
     print()
 print()
     
@@ -151,5 +151,137 @@ for x in range(1,6):
         else:
             print(" ",end="")
     print()
-    
 
+# 6. Mirrored Rhombus Pattern
+
+#  *****
+#   *****
+#    *****
+#     *****
+#      *****
+
+for x in range(1,6):
+    
+    #space
+    for z in range(1,x+1):
+        print(" ",end="")
+    
+    #star
+    for y in range(1,6):
+        print("*",end="")
+    print()
+print()
+
+# 7.Right Triangle Pattern
+
+# *
+# **
+# ***
+# ****
+# *****
+
+for x in range(1,6):
+    for y in range(1,x+1):
+        print("*",end="")
+    print()
+print()
+
+# 8.Hollow Right Triangle Pattern
+# *
+# **
+# * *
+# *  *
+# *****
+
+for x in range(1,6):
+    for y in range(1,x+1):
+        if(x==1 or x==5 or y==1 or y==5 or x==y):
+          print("*",end="")
+        else:
+            print(" ",end="")  
+    print()
+
+# 9.Mirrored Right Triangle Pattern
+#      *
+#     **
+#    ***
+#   ****
+#  *****
+
+for x in range(1,6):
+    #space
+    for z in range(x,6):
+        print(" ",end="")
+    #star
+    for y in range(1,x+1):
+        print("*",end="")
+    print()
+print()
+
+# 10.Hollow Mirrored Right Triangle Pattern
+#      *
+#     **
+#    * *
+#   *  *
+#  *****
+
+for x in range(1,6):
+    
+    #space
+    for z in range(x,6):
+        print(" ",end="")
+        
+    #star
+    for y in range(1,x+1):
+        if (x==1 or x==5 or y==1 or y==5 or x==y):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+print()
+
+# 11. Inverted Right Triangle Pattern
+# *****
+# ****
+# ***
+# **
+# *
+
+for x in range(1,6):
+    for y in range(x,6):
+        print("*",end="")
+    print()
+
+# 12. Hollow Inverted Right Triangle Pattern
+
+# *****
+# *  *
+# * *
+# **
+# *
+
+for x in range(1,6):
+    for y in range(x,6):
+        if(x==1 or x==5 or y==1 or y==5 or x==y):
+            print("*",end="")
+        else:
+            print(" ",end="")  
+    print()
+print()
+
+# 13. Mirrored Inverted Right Triangle
+
+# *****
+#  ****
+#   ***
+#    **
+#     *
+
+for x in range(1,6):
+    #star
+
+
+
+
+        
+        
